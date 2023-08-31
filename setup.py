af618643dd2ff99883727688990764b6898e2b7c
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2023-08-31 21:27:38 +1000

    add build commands

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.0'
+DESCRIPTION = 'AutoCAD Drafting using AI'
+
+# Setting up
+setup(
+    name="aicaddrafter",
+    version=VERSION,
+    author="mattcoulter7 (Matt Coulter)",
+    author_email="<mattcoul7@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'ai', 'cad', 'autocad', 'drafter'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
